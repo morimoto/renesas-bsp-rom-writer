morimoto/renesas-bsp-rom-writer

commit 477459908c16b8c577c58d228de024bd1f408036

Add new script
Current ROM writer is unfortunately not so user-friendly,
thus, it looks very complex for beginner user.
The issue is that beginner is easy to confuse/misunderstand
by many reasons.

1st reason is that ROM writer command requests select option
for Yocto or for Android which is used to know the nessesary
ROM file and its load postion

In Yocto case, using ROM writer is everything, but in Android case,
it needs 2 steps. 1st is writing ROM, and 2nd is Android settings.

But beginner will misunderstand that ROM writer command will setup
all Android settings because he selected Android via option though
it needs other Android setting command.
Of couse these are explained in README, but beginner doesn't read it.

2nd reason is that, many limitation/note/prepare are needed and these
are indicated at README. But it has many READMEs. for board, for linux,
for windows, etc. And same as 1st reason, beginner doesn't read it..

Becase ROM writing needs many steps and many limitation/note,
interrctive command is better idea from beginner and/or user-friendly
point of view.

This patch adds new style of script for it.

The new script will indicate nesessary message at nesessary timing,
thus user doesn't need to check README.
And also it is easy to adjust to new style board (Current script is
deeply depends on StarterKit/Salvator style).

Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>

Makefile
@@ -1,0 +1,29 @@
+FLASH_WRITER=script/flash_writer
+GCC32=gcc-linaro-7.3.1-2018.05-x86_64_arm-eabi
+GCC64=gcc-linaro-7.3.1-2018.05-x86_64_aarch64-elf
+GCC32_PRE=arm-eabi
+GCC64_PRE=aarch64-elf
+GCC32_URL=https://releases.linaro.org/components/toolchain/binaries/7.3-2018.05/${GCC32_PRE}/${GCC32}.tar.xz
+GCC64_URL=https://releases.linaro.org/components/toolchain/binaries/7.3-2018.05/${GCC64_PRE}/${GCC64}.tar.xz
+MOT_STARTERKIT=${FLASH_WRITER}/AArch32_output/AArch32_Flash_writer_SCIF_DUMMY_CERT_E6300400_ULCB.mot
+MOT_SALVATOR=${FLASH_WRITER}/AArch64_output/AArch64_Flash_writer_SCIF_DUMMY_CERT_E6300400_salvator-x.mot
+
+all: ${FLASH_WRITER} ${MOT_STARTERKIT} ${MOT_SALVATOR}
+
+# see ${renesas-bsp-rom-writer}/starterkit/confirm/mot
+# why we can't use 64bit gcc on Starterkit
+${MOT_STARTERKIT}: script/${GCC32}/bin/${GCC32_PRE}-gcc
+	@cd ${FLASH_WRITER};\
+	CROSS_COMPILE=../${GCC32}/bin/${GCC32_PRE}- make AArch=32 BOARD=ULCB
+
+${MOT_SALVATOR}: script/${GCC64}/bin/${GCC64_PRE}-gcc
+	@cd ${FLASH_WRITER};\
+	CROSS_COMPILE=../${GCC64}/bin/${GCC64_PRE}- make AArch=64
+
+script/${GCC32}/bin/${GCC32_PRE}-gcc:
+	@cd script; wget ${GCC32_URL}; tar xvf ${GCC32}.tar.xz
+script/${GCC64}/bin/${GCC64_PRE}-gcc:
+	@cd script; wget ${GCC64_URL}; tar xvf ${GCC64}.tar.xz
+
+${FLASH_WRITER}:
+	@git clone https://github.com/renesas-rcar/flash_writer.git ${FLASH_WRITER}

commit a151f0c9fe4d8e1f96c7750ec1e6a7eadb9175cc

Makefile: Fix reference to starterkit/config/mot
The Makefile comments refer to a file that does not exist.

Fixes: 477459908c16b8c5 ("Add new script")
Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>
Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>

Makefile
@@ -10,7 +10,7 @@ MOT_SALVATOR=${FLASH_WRITER}/AArch64_output/AArch64_Flash_writer_SCIF_DUMMY_CERT
 
 all: ${FLASH_WRITER} ${MOT_STARTERKIT} ${MOT_SALVATOR}
 
-# see ${renesas-bsp-rom-writer}/starterkit/confirm/mot
+# see ${renesas-bsp-rom-writer}/starterkit/config/mot
 # why we can't use 64bit gcc on Starterkit
 ${MOT_STARTERKIT}: script/${GCC32}/bin/${GCC32_PRE}-gcc
 	@cd ${FLASH_WRITER};\