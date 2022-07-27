bb5727c05b1c46e871e9d5c87abba6e9cdadc5ab
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2022-07-27 11:11:53 +0900

    Add V3H2 Condor-I support
    This patch adds V3H2 Condor-I ROM Writer support.
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>

---
+++ script/python/condor.py
@@ -0,0 +1,118 @@
+#! /usr/bin/env python3
+#===============================
+#
+# condor
+#
+# 2022/07/27 Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
+#===============================
+import os
+import sys
+import time
+
+import base
+#====================================
+#
+# board
+#
+#====================================
+class board(base.board):
+    #--------------------
+    # mot_file
+    # mot_error
+    #--------------------
+    def mot_file(self):
+        return self.ttm_array(self.map(), "mot_file")[0]
+
+    #--------------------
+    # select_soc
+    #--------------------
+    def __select_soc(self):
+        self.__select_soc_noselect()
+
+    #--------------------
+    # __init__
+    #--------------------
+    def __init__(self, ver="", tty="", mac=None):
+
+        super().__init__("condor", soc="v4h2", os="sdk", ver=ver, tty=tty, mode="normal")
+
+        self.confirm_location()
+        self.config_load()
+        self.setup()
+
+        self.confirm_info()
+        self.config_save()
+        self.check_files()
+
+#====================================
+#
+# rom_write_guide
+#
+#====================================
+class rom_write_guide(base.guide):
+
+    #--------------------
+    # __init__
+    #--------------------
+    def __init__(self, board):
+        super().__init__(board.tty(), 115200)
+        # possible to use from child-class
+        self.board = board
+
+    #--------------------
+    # guide_start
+    #--------------------
+    def guide_start(self):
+        sw = base.switch(self.board.dir_config("sw"))
+
+        # chech mot file
+        mot_file = self.board.mot_file()
+        if (not os.path.exists("{}/{}".format(self.cwd(), mot_file))):
+            self.msg("It seems you don't have necessary mot file\n"\
+                     "({})\n".format(mot_file) +\
+                     "Please re-check current dir")
+            sys.exit(1)
+
+        # power off
+        self.power("OFF")
+        self.ask_yn()
+
+        # indicate dip-switch update mode
+        sw.print_msg_update()
+        self.ask_yn()
+
+        self.power("ON")
+        self.expect("please send !")
+        self.send_file("{}/{}".format(self.cwd(), mot_file))
+        self.expect(">")
+
+        # main loop
+        self.sk_type_main_loop(self.board.addr_map(), "3", 2)
+
+        # power off
+        self.power("OFF")
+        self.ask_yn()
+
+        # indicate dip-switch normal mode
+        sw.print_msg_normal()
+        self.ask_yn()
+
+        # baudrate settings
+        self.msg("finished !!")
+
+#====================================
+#
+# As command
+#
+#	> condor	# test
+#	> condor sdk	# SDK
+#
+#====================================
+if __name__=='__main__':
+    if (len(sys.argv) < 2):
+        # test
+        board(ver="3.3.0", tty="/dev/ttyUSB0")
+    elif (sys.argv[1] == "sdk"):
+        rom_write_guide(board()).guide_start()
+    else:
+        print("unknown command")
