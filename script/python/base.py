morimoto/renesas-bsp-rom-writer

commit 67027ac16400287d31866d2f22feb47d61eff8ba

Create new board dir, and add SoC as prefix
This patch creates new board/ dir and move all
board settings under to it.
Then, each board have SoC as prefix

Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>

script/python/base.py
@@ -296,15 +296,14 @@ class board(base):
     #--------------------
     # __init__
     #--------------------
-    def __init__(self, board,
-                 soc=None, os=None, ver=None, tty=None, mode=None, mac=None):
+    def __init__(self, soc=None, rom=None, ver=None, tty=None, mode="normal", mac=None):
 
         # None   : not use
         # ""     : be used, but not yet selected
         # "xxx"  : be used, and selected
-        self.__board	= board
+        self.__board	= os.path.splitext(os.path.basename(sys.argv[0]))[0]
         self.__soc	= soc
-        self.__os	= os
+        self.__rom	= rom
         self.__ver	= ver
         self.__tty	= tty
         self.__mode	= mode		# normal, mot
@@ -313,7 +312,7 @@ class board(base):
         self.__mac	= mac
 
         # for inside
-        self.__config	= ".renesas_bsp_rom_writer.{}".format(board)
+        self.__config	= ".renesas_bsp_rom_writer.{}".format(self.__board)
         self.__addr_map	= None
         self.__emmc_map	= None
         self.__map	= None
@@ -341,7 +340,7 @@ class board(base):
     def board(self):	return self.__board
     def tty(self):	return self.__tty
     def soc(self):	return self.__soc
-    def os(self):	return self.__os
+    def rom(self):	return self.__rom
     def map(self):	return self.__map
     def addr_map(self):	return self.__addr_map
     def emmc_map(self):	return self.__emmc_map
@@ -366,9 +365,9 @@ class board(base):
     #--------------------
     def dir_board(self, path="", full=1):
         dir = "{}/".format(self.top()) if (full) else ""
-        return "{}{}/{}".format(dir, self.__board, path)
+        return "{}board/{}/{}".format(dir, self.__board, path)
     def dir_config(self, path="", full=1):	return self.dir_board("config/" + path, full)
-    def dir_config_os(self, path="", full=1):	return self.dir_config("os/{}/{}".format(self.__os, path), full)
+    def dir_config_rom(self, path="", full=1):	return self.dir_config("rom/{}/{}".format(self.__rom, path), full)
     def dir_config_sw(self, path="", full=1):	return self.dir_config("sw/" + path, full)
 
     #--------------------
@@ -393,7 +392,7 @@ class board(base):
         # __init__() set default value
         # lood config if value was ""
         if (self.__soc  == ""): self.__soc  = self.config_read("soc")
-        if (self.__os   == ""): self.__os   = self.config_read("os")
+        if (self.__rom  == ""): self.__rom  = self.config_read("rom")
         if (self.__ver  == ""): self.__ver  = self.config_read("version")
         if (self.__tty  == ""): self.__tty  = self.config_read("tty")
         if (self.__mac  == ""): self.__mac  = self.config_read("mac")
@@ -401,7 +400,7 @@ class board(base):
 
     def config_save(self):
         if (self.__soc  is not None): self.config_write("soc",     self.__soc)
-        if (self.__os   is not None): self.config_write("os",      self.__os)
+        if (self.__rom  is not None): self.config_write("rom",     self.__rom)
         if (self.__ver  is not None): self.config_write("version", self.__ver)
         if (self.__tty  is not None): self.config_write("tty",     self.__tty)
         if (self.__mac  is not None): self.config_write("mac",     self.__mac)
@@ -414,7 +413,7 @@ class board(base):
     # if default select_xx() was not good match
     #--------------------
     def setup(self):
-        if (self.__os   is not None): self.__select_os()
+        if (self.__rom  is not None): self.__select_rom()
         if (self.__ver  is not None): self.__select_ver()
         if (self.__soc  is not None): self.__select_soc()
         if (self.__tty  is not None): self.__select_tty()
@@ -422,20 +421,20 @@ class board(base):
         if (self.__mode is not None): self.__select_mode()
 
     #--------------------
-    # select_os (default)
+    # select_rom (default)
     #--------------------
-    def __select_os(self):
-        # check os/${os}/config file
-        while (not os.path.exists(self.dir_config_os("config"))):
-            self.__os = self.select("Select write OS", self.runl("ls {}".format(self.dir_config("os"))))
+    def __select_rom(self):
+        # check rom/${os}/config file
+        while (not os.path.exists(self.dir_config_rom("config"))):
+            self.__rom = self.select("Select write OS", self.runl("ls {}".format(self.dir_config("rom"))))
 
     #--------------------
     # select_ver (default)
     #--------------------
     def __select_ver(self):
-        list_version = self.ttm_array(self.dir_config_os("config"), "list_version")
+        list_version = self.ttm_array(self.dir_config_rom("config"), "list_version")
         while (not self.__ver in list_version):
-            self.__ver = self.select("Select [{}] Version".format(self.os()), list_version)
+            self.__ver = self.select("Select [{}] Version".format(self.rom()), list_version)
 
     #--------------------
     # select_soc (default)
@@ -445,13 +444,13 @@ class board(base):
 
     def __select_soc(self):
         list_soc     = self.ttm_array(self.dir_config("soc"), "list_soc")
-        list_version = self.ttm_array(self.dir_config_os("config"), "list_version")
-        list_map     = self.ttm_array(self.dir_config_os("config"), "list_map")
+        list_version = self.ttm_array(self.dir_config_rom("config"), "list_version")
+        list_map     = self.ttm_array(self.dir_config_rom("config"), "list_map")
 
         if (not self.__ver in list_version):
             self.error("select version first")
 
-        dir_map = self.dir_config_os(list_map[list_version.index(self.__ver)])
+        dir_map = self.dir_config_rom(list_map[list_version.index(self.__ver)])
         text = self.soc_explanation() + "\n\nSelect SoC/WS ROM\n"
 
         while (not os.path.isfile("{}/{}".format(dir_map, self.__soc))):
@@ -465,12 +464,12 @@ class board(base):
     # select_soc_noselect
     #--------------------
     def __select_soc_noselect(self):
-        list_version = self.ttm_array(self.dir_config_os("config"), "list_version")
-        list_map     = self.ttm_array(self.dir_config_os("config"), "list_map")
+        list_version = self.ttm_array(self.dir_config_rom("config"), "list_version")
+        list_map     = self.ttm_array(self.dir_config_rom("config"), "list_map")
 
         map = list_map[list_version.index(self.__ver)]
 
-        self.__map = self.dir_config_os(map)
+        self.__map = self.dir_config_rom(map)
         self.__addr_map = addr_map(self.__map)
         self.__emmc_map = emmc_map(self.__map)
 
@@ -478,7 +477,7 @@ class board(base):
     # select_tty (default)
     #--------------------
     def tty_connection(self):
-        return "\n".join(self.ttm_array(self.dir_config_os("connection"), "tty_connection"))
+        return "\n".join(self.ttm_array(self.dir_config_rom("connection"), "tty_connection"))
 
     def __tty_error(self):
         if (not os.path.exists(self.__tty)):
@@ -581,7 +580,7 @@ class board(base):
 
         deep = 0
         if (self.__soc  is not None): text += "  [SoC/WS]:  {}\n".format(self.__soc)
-        if (self.__os   is not None): text += "  [OS]:      {}\n".format(self.__os)
+        if (self.__rom  is not None): text += "  [OS]:      {}\n".format(self.__rom)
         if (self.__ver  is not None): text += "  [Version]: {}\n".format(self.__ver)
         if (self.__mode is not None): text += "  [Mode]:    {}\n".format(self.__mode)
         if (self.__tty  is not None): text += "* [TTY]:     {}\n".format(self.__tty); deep = 1
@@ -610,10 +609,10 @@ class board(base):
         # If not exist, confirm_location
         if (os.path.exists(self.config_file())): return
 
-        self.msg("This script requires be called from {} ROM directory.\n".format(self.os()) +\
+        self.msg("This script requires be called from {} ROM directory.\n".format(self.rom()) +\
                  "Are you calling this script from there ?\n\n" +\
-                 "  > cd ${{{0} ROM dir}}\n".format(self.os()) +\
-                 "  > ${{renesas-bsp-rom-writer}}/{}/linux/{}-writer".format(self.board(), self.os()))
+                 "  > cd ${{{0} ROM dir}}\n".format(self.rom()) +\
+                 "  > ${{renesas-bsp-rom-writer}}/board/{}/linux/{}-writer".format(self.board(), self.rom()))
         self.ask_yn(quit=True)
 
     #--------------------
@@ -625,7 +624,7 @@ class board(base):
             if (self.ask_yn()): break;
 
             # reset all setting
-            # ignore os here
+            # ignore rom here
             if (self.__soc  is not None): self.__soc  = ""
             if (self.__ver  is not None): self.__ver  = ""
             if (self.__tty  is not None): self.__tty  = ""
@@ -972,7 +971,7 @@ if __name__=='__main__':
         #
         # addr_map
         #
-        map = addr_map(b.top() + "/starterkit/config/os/yocto/map01/h3_4g")
+        map = addr_map(b.top() + "/starterkit/config/rom/yocto/map01/h3_4g")
 
         print("addr      save    srec")
         for i in range(map.len()):

script/python/gen3_salvator.py
@@ -1,0 +1,176 @@
+#! /usr/bin/env python3
+#===============================
+#
+# gen3-salvator
+#
+# 2022/01/19 Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
+#===============================
+import sys
+
+import base
+import gen3_starterkit
+#====================================
+#
+# board
+#
+#====================================
+class board(base.board):
+    #--------------------
+    # mot_file
+    # mot_error
+    #--------------------
+    def mot_init(self):
+        # mot setting file for dir_config()
+        # ${renesas-bsp-rom-writer}/starterkit/config/mot
+        mot_config = self.dir_config("mot")
+
+        # because Windws path is not same as Linux,
+        # we need to call replace() to exchange \ to /
+        self.__mot = "{}/{}".format(
+            self.ttm_array(mot_config, "dir_mot")[0].replace("\\", "/"),
+            self.ttm_array(mot_config, "mot")[0])
+
+    def mot_file(self):
+        return "{}/{}".format(self.top(), self.__mot)
+
+    def mot_error(self):
+        self.error("You selected mot mode to write ROM.\n"\
+                   "It needs mot file, but you didn't create it yet.\n"\
+                   "Please run make and create mot file.\n\n"\
+                   "   > cd ${renesas-bsp-rom-writer}\n"\
+                   "   > make\n\n"\
+                   "You need is\n" + self.mot_file())
+
+    #--------------------
+    # __init__
+    #--------------------
+    def __init__(self, confirm, soc="", rom="", ver="", tty="", mac=None):
+
+        # mac is needed if Android
+        if (rom == "android"):
+            mac = ""
+
+        super().__init__(soc=soc, rom=rom, ver=ver, tty=tty, mac=mac, mode="")
+
+        self.mot_init()
+
+        self.confirm_location()
+        self.config_load()
+        self.setup()
+
+        # confirm to user
+        if (confirm):
+            self.confirm_info()
+        self.config_save()
+        self.check_files()
+
+    #--------------------
+    # mode_explanation
+    #--------------------
+    def mode_explanation(self):
+        # We can't share mode explanation message on config file
+        # because Linux vs Windows explanation are different
+        return "normal: Need Dip-Switch settings at both Front/Back side\n" +\
+               "        to write ROM. You need to select normal mode\n" +\
+               "        if your current board has no ROM (no U-boot).\n" +\
+               "mot:    Need Dip-Switch settings at only Front side.\n" +\
+               "        You need to run make and create mot first.\n" +\
+               "             > cd ${renesas-bsp-rom-writer}\n" +\
+               "             > make\n"
+
+#====================================
+#
+# rom_write_guide
+#
+# Salvator / Starterkit are almost same.
+# reuse Starterkit guide with some overwrite
+#====================================
+class rom_write_guide(gen3_starterkit.rom_write_guide):
+
+    #======================
+    # <Overwrite>
+    #
+    # load_sw
+    #
+    # salvator sw setting are located at
+    # ${renesas-bsp-rom-writer}/starterkit/config/sw/${mode}
+    #======================
+    def load_sw(self):
+        return base.switch(self.board.dir_config_sw(self.board.mode()))
+
+    #======================
+    # <Overwrite>
+    #
+    # Salvator always need 2nd Y in main loop
+    #======================
+    def yes_loop_num(self):
+        return 2
+
+    #======================
+    # <Overwrite>
+    #
+    # guide_for_mot
+    #======================
+    def guide_for_mot(self):
+        sw = self.load_sw()
+
+        # indicate dip-switch update mode
+        sw.print_msg_update()
+        self.ask_yn()
+
+        # power on
+        self.power("ON")
+        self.expect("please send !")
+
+        # indicate meesage
+        # and send mot file
+        self.send_file(self.board.mot_file())
+        self.expect(">")
+
+        # speed up
+        self.speed_up("921.6Kbps", 921600)
+
+        # main loop
+        self.main_loop()
+
+        # back dip-switch normal mode
+        sw.print_msg_normal()
+        self.ask_yn()
+
+        self.msg("finished !!")
+
+#====================================
+#
+# As command
+#
+#	> salvator yocto		# yocto
+#	> salvator android		# android
+#	> salvator --			# select os
+#	> salvator android_fastboot	# android fastboot for uboot
+#	> salvator			# test
+#
+#====================================
+if __name__=='__main__':
+    confirm	= 1
+    rom		= ""
+
+    if (len(sys.argv) < 2):
+        # test
+        board(confirm, soc="h3_4g", rom="yocto", ver="5.5.0", tty="/dev/ttyUSB0")
+        sys.exit(0)
+    if (sys.argv[1] == "yocto"):
+        rom = "yocto"
+        guide = rom_write_guide
+    elif (sys.argv[1] == "android"):
+        rom = "android"
+        guide = rom_write_guide
+    elif (sys.argv[1] == "android_fastboot"):
+        confirm = 0
+        rom = "android"
+        # reuse starterkit fastboot
+        guide = starterkit.fastboot_uboot_guide
+    else:
+        print("unknown command")
+        sys.exit(1)
+
+    guide(board(confirm, rom=rom)).guide_start()

script/python/gen3_starterkit.py
@@ -1,0 +1,346 @@
+#! /usr/bin/env python3
+#===============================
+#
+# gen3-starterkit
+#
+# 2022/01/07 Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
+#===============================
+import sys
+import time
+
+import base
+#====================================
+#
+# board
+#
+#====================================
+class board(base.board):
+    #--------------------
+    # mot_file
+    # mot_error
+    #--------------------
+    def mot_init(self):
+        # mot setting file for dir_config()
+        # ${renesas-bsp-rom-writer}/starterkit/config/mot
+        mot_config = self.dir_config("mot")
+
+        # because Windws path is not same as Linux,
+        # we need to call replace() to exchange \ to /
+        self.__mot = "{}/{}".format(
+            self.ttm_array(mot_config, "dir_mot")[0].replace("\\", "/"),
+            self.ttm_array(mot_config, "mot")[0])
+
+    def mot_file(self):
+        return "{}/{}".format(self.top(), self.__mot)
+
+    def mot_error(self):
+        self.error("You selected mot mode to write ROM.\n"\
+                   "It needs mot file, but you didn't create it yet.\n"\
+                   "Please run make and create mot file.\n\n"\
+                   "   > cd ${renesas-bsp-rom-writer}\n"\
+                   "   > make\n\n"\
+                   "You need is\n" + self.mot_file())
+
+    #--------------------
+    # __init__
+    #--------------------
+    def __init__(self, confirm, soc="", rom="", ver="", tty="", mac=None):
+
+        # mac is needed if Android
+        if (rom == "android"):
+            mac = ""
+
+        super().__init__(soc=soc, rom=rom, ver=ver, tty=tty, mac=mac, mode="")
+
+        self.mot_init()
+
+        self.confirm_location()
+        self.config_load()
+        self.setup()
+
+        # confirm to user
+        if (confirm):
+            self.confirm_info()
+        self.config_save()
+        self.check_files()
+
+    #--------------------
+    # mode_explanation
+    #--------------------
+    def mode_explanation(self):
+        # We can't share mode explanation message on config file
+        # because Linux vs Windows explanation are different
+        return "normal: Need manual Dip-Switch settings to write ROM.\n" +\
+               "        You need to select normal mode\n" +\
+               "        if your current board has no ROM (no U-boot)\n" +\
+               "mot:    No Dip-Switch settings are needed.\n" +\
+               "        You need to run make and create mot first.\n" +\
+               "             > cd ${renesas-bsp-rom-writer}\n" +\
+               "             > make\n"
+
+#====================================
+#
+# rom_write_guide
+#
+#====================================
+class rom_write_guide(base.guide):
+
+    #======================
+    # <Overwrite>
+    #
+    # load_sw
+    #
+    # starterkit sw setting are located at
+    # ${renesas-bsp-rom-writer}/starterkit/config/sw/${soc}
+    #
+    # Overwrite load_sw() if other board re-use rom_write_guide
+    #======================
+    def load_sw(self):
+        return base.switch(self.board.dir_config_sw(self.board.soc()))
+
+    #======================
+    # <Overwrite>
+    #
+    # M2 v1 Starterkit doesn't need 2nd Y in main loop
+    # Overwrite yes_loop_num() if other board re-use rom_write_guide
+    #======================
+    def yes_loop_num(self):
+        if (self.board.soc_ws() == "m3_2g"):
+            return 1
+        return 2
+
+    #--------------------
+    # __init__
+    #--------------------
+    def __init__(self, board):
+        super().__init__(board.tty(), 115200)
+        # possible to use from child-class
+        self.board = board
+
+        self.__yes_loop = self.yes_loop_num()
+
+    #--------------------
+    # main loop
+    #--------------------
+    def main_loop(self):
+        self.sk_type_main_loop(self.board.addr_map(), "3", self.__yes_loop, self.ask_loop())
+
+    #--------------------
+    # guide_for_mot
+    #--------------------
+    def guide_for_mot(self):
+        mot_config = "{}/starterkit/config/mot".format(self.top())
+        cpld_cmd = self.ttm_array(mot_config, "cpld_cmd")
+
+        # power on
+        # and stop the U-Boot autorun
+        self.power("ON")
+        self.stop_autorun()
+
+        # just to be sure
+        self.send()
+        self.expect("=>")
+
+        # send CPLD settings
+        for cmd in cpld_cmd:
+            self.send(cmd)
+            self.expect("=>")
+
+        # indicate meesage
+        # and send mot file
+        self.expect("please send !")
+        self.send_file(self.board.mot_file())
+        self.expect(">")
+
+        # speed up
+        self.speed_up("921.6Kbps", 921600)
+
+        # main loop
+        self.main_loop()
+        self.msg("finished !!")
+
+    #--------------------
+    # guide_for_normal
+    #--------------------
+    def guide_for_normal(self):
+        sw = self.load_sw()
+
+        # indicate dip-switch update mode
+        sw.print_msg_update()
+        self.ask_yn()
+
+        self.power("ON")
+        self.expect(">")
+
+        # indicate dip-switch normal mode
+        sw.print_msg_normal()
+        self.ask_yn()
+
+        # speed up
+        self.speed_up("921.6Kbps", 921600)
+
+        # main loop
+        self.main_loop()
+        self.msg("finished !!")
+
+    #--------------------
+    # guide_start
+    #--------------------
+    def guide_start(self):
+        self.msg("Confirm serial connection.\n"\
+                 "Please don't use USB hub between [PC] and [board].\n"\
+                 "       ^^^^^^^^^^^^^^^^^\n\n" +\
+                 self.board.tty_connection())
+        self.ask_yn()
+
+        # power off
+        self.power("OFF")
+        self.ask_yn()
+
+        # normal/mot mode init
+        if (self.board.mode() == "mot"):
+            self.guide_for_mot()
+        else:
+            self.guide_for_normal()
+
+#====================================
+#
+# fastboot_uboot_guide
+#
+#====================================
+class fastboot_uboot_guide(base.guide):
+
+    #--------------------
+    # __init__
+    #--------------------
+    def __init__(self, board):
+        super().__init__(board.tty(), 115200)
+
+        # possible to use from child-class
+        self.board = board
+
+    #--------------------
+    # erase_bootloader
+    #--------------------
+    def erase_bootloader(self):
+        self.stop_autorun()
+
+        self.send()
+        self.expect("=>")
+
+        self.send("mmc dev 1 1")
+        self.expect("=>")
+
+        self.send("mw.b 4f000000 0 200000")
+        self.expect("=>")
+
+        self.send("mmc write 4f000000 0 1000")
+        self.expect("=>")
+
+        self.send("mmc dev 1 2")
+        self.expect("=>")
+
+        self.send("mw.b 4f000000 0 200000")
+        self.expect("=>")
+
+        self.send("mmc write 4f000000 0 1000")
+        self.expect("=>")
+
+        self.send("reset")
+
+        time.sleep(1)
+        self.stop_autorun()
+
+    #--------------------
+    # setenv
+    #--------------------
+    def setenv(self):
+        self.send()
+        self.expect("=>")
+
+        self.send("env default -a")
+        self.expect("=>")
+
+        self.send("setenv ethaddr {}".format(self.board.mac()))
+        self.expect("=>")
+
+        # use vague number for serialno :)
+        self.send("setenv serialno 00009999")
+        self.expect("=>")
+
+        self.send("setenv bootargs video=LVDS-1:d video=VGA-1:d init_time={}".format(int(time.time())))
+        self.expect("=>")
+
+        self.send("saveenv")
+        self.expect("=>")
+
+        self.send("reset")
+
+        time.sleep(1)
+        self.stop_autorun()
+
+    #--------------------
+    # start_fastboot
+    #--------------------
+    def start_fastboot(self):
+        self.send()
+        self.expect("=>")
+
+        self.send("fastboot")
+        self.expect("USB")
+
+    #--------------------
+    # guide_start
+    #--------------------
+    def guide_start(self):
+        self.msg("Confirm serial connection.\n"\
+                 "Please don't use USB hub between [PC] and [board].\n"\
+                 "       ^^^^^^^^^^^^^^^^^\n\n" +\
+                 self.board.tty_connection())
+        self.ask_yn()
+
+        # power off
+        self.power("OFF")
+        self.ask_yn()
+
+        # power off
+        self.power("ON")
+
+        self.erase_bootloader()
+        self.setenv()
+        self.start_fastboot()
+
+#====================================
+#
+# As command
+#
+#	> starterkit yocto		# yocto
+#	> starterkit android		# android
+#	> starterkit --			# select os
+#	> starterkit android_fastboot	# android fastboot for uboot
+#	> starterkit			# test
+#
+#====================================
+if __name__=='__main__':
+    confirm	= 1
+    rom		= ""
+
+    if (len(sys.argv) < 2):
+        # test
+        board(confirm, soc="h3_4g", rom="yocto", ver="5.5.0", tty="/dev/ttyUSB0")
+        sys.exit(0)
+    if (sys.argv[1] == "yocto"):
+        rom = "yocto"
+        guide = rom_write_guide
+    elif (sys.argv[1] == "android"):
+        rom= "android"
+        guide = rom_write_guide
+    elif (sys.argv[1] == "android_fastboot"):
+        confirm = 0
+        rom = "android"
+        guide = fastboot_uboot_guide
+    else:
+        print("unknown command")
+        sys.exit(1)
+
+    guide(board(confirm, rom=rom)).guide_start()

commit bb40873123b6b6796361a8306905b2d173860888

script/python: fixup path
commit 67027ac1640 ("Create new board dir, and add SoC as prefix")
changed board settings path, but some of them are still using
old path. This patch fixup it.

Fixes: 67027ac1640 ("Create new board dir, and add SoC as prefix")
Reported-by: Harunobu Kurokawa <harunobu.kurokawa.dn@renesas.com>
Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>

script/python/base.py
@@ -964,14 +964,14 @@ if __name__=='__main__':
         #
         # switch
         #
-        sw = switch(b.top() + "/starterkit/config/sw/h3")
+        sw = switch(b.top() + "/board/gen3_starterkit/config/sw/h3")
         sw.print_msg_update()
         sw.print_msg_normal()
     elif (sys.argv[1] == "map"):
         #
         # addr_map
         #
-        map = addr_map(b.top() + "/starterkit/config/rom/yocto/map01/h3_4g")
+        map = addr_map(b.top() + "board/gen3_starterkit/config/rom/yocto/map01/h3_4g")
 
         print("addr      save    srec")
         for i in range(map.len()):

script/python/gen3_salvator.py
@@ -21,7 +21,7 @@ class board(base.board):
     #--------------------
     def mot_init(self):
         # mot setting file for dir_config()
-        # ${renesas-bsp-rom-writer}/starterkit/config/mot
+        # ${renesas-bsp-rom-writer}/board/gen3_starterkit/config/mot
         mot_config = self.dir_config("mot")
 
         # because Windws path is not same as Linux,
@@ -93,7 +93,7 @@ class rom_write_guide(gen3_starterkit.rom_write_guide):
     # load_sw
     #
     # salvator sw setting are located at
-    # ${renesas-bsp-rom-writer}/starterkit/config/sw/${mode}
+    # ${renesas-bsp-rom-writer}/board/gen3_starterkit/config/sw/${mode}
     #======================
     def load_sw(self):
         return base.switch(self.board.dir_config_sw(self.board.mode()))

script/python/gen3_starterkit.py
@@ -21,7 +21,7 @@ class board(base.board):
     #--------------------
     def mot_init(self):
         # mot setting file for dir_config()
-        # ${renesas-bsp-rom-writer}/starterkit/config/mot
+        # ${renesas-bsp-rom-writer}/board/gen3_starterkit/config/mot
         mot_config = self.dir_config("mot")
 
         # because Windws path is not same as Linux,
@@ -91,7 +91,7 @@ class rom_write_guide(base.guide):
     # load_sw
     #
     # starterkit sw setting are located at
-    # ${renesas-bsp-rom-writer}/starterkit/config/sw/${soc}
+    # ${renesas-bsp-rom-writer}/board/gen3_starterkit/config/sw/${soc}
     #
     # Overwrite load_sw() if other board re-use rom_write_guide
     #======================
@@ -129,7 +129,7 @@ class rom_write_guide(base.guide):
     # guide_for_mot
     #--------------------
     def guide_for_mot(self):
-        mot_config = "{}/starterkit/config/mot".format(self.top())
+        mot_config = self.board.dir_config("mot")
         cpld_cmd = self.ttm_array(mot_config, "cpld_cmd")
 
         # power on