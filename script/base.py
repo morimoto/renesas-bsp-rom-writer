morimoto/renesas-bsp-rom-writer

commit 0c88f2a49c22099270da555270856d1e2a061779

x5h_ironhide: update auto_cmd
Current auto_cmd requests to use udev rule, but it will conflict if PC
connects multi RL78 / X5H boards.
This patch allow to use specific USB serial for auto_cmd.

"rcar_board_control_x5h" command requests TTY by this patch

	- rcar_board_control_x5h on
	+ rcar_board_control_x5h /dev/ttyUSBx on
	                         ^^^^^^^^^^^^

[auto_cmd] is removed, and [auto_cmd_tty] is added in
.renesas_bsp_rom_writer.x5h_ironhide

	- [auto_cmd]:rcar_board_control_x5h
	+ [auto_cmd_tty]:/dev/ttyUSBx

Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
Acked-by: Adrien Ricciardi <aricciardi@baylibre.com>
Tested-by: Adrien Ricciardi <aricciardi@baylibre.com>

script/base.py
@@ -277,7 +277,7 @@ class board(base):
     #--------------------
     # init
     #--------------------
-    def init(self, soc=None, rom=None, ver=None, tty=None, board=None, mode="normal", baudrate=115200):
+    def init(self, soc=None, rom=None, ver=None, tty=None, board=None, mode="normal", baudrate=115200, auto_cmd=None):
 
         # None   : not use
         # ""     : be used, but not yet selected
@@ -297,7 +297,12 @@ class board(base):
         self.__config	= ".renesas_bsp_rom_writer.{}".format(self.__board)
         self.__addr_map	= {}
         self.__map	= None
-        self.__auto_cmd = None
+
+        # for auto command
+        self.__auto_cmd = auto_cmd	# path from ${TOP}/board/
+        self.__auto_cmd_tty = None	# None:		not used
+                                  	# auto:		use udev
+                                  	# /dev/ttyXX:	use specified serial
 
         self.confirm_location()
         self.config_load()
@@ -400,9 +405,14 @@ class board(base):
         if (self.__tty  == ""): self.__tty  = self.config_read("tty")
         if (self.__mode == ""): self.__mode = self.config_read("mode")
 
-        # The auto_cmd is specific to a test bench and not meant to be user selectable
-        self.__auto_cmd = self.config_read("auto_cmd")
-        if (self.__auto_cmd == ""): self.__auto_cmd = None
+        # The auto_cmd is specific to each board
+        if (self.__auto_cmd is not None):
+            self.__auto_cmd_tty = self.config_read("auto_cmd_tty")
+            if (self.__auto_cmd_tty == ""):
+                self.__auto_cmd_tty = None
+            else:
+                if (self.__tty_error(self.__auto_cmd_tty)):
+                    self.error("[auto_cmd_tty](= {}) is not valid tty\n".format(self.__auto_cmd_tty))
 
     def config_save(self):
         if (self.__soc  is not None): self.config_write("soc",     self.__soc)
@@ -606,7 +616,9 @@ class board(base):
         if (self.__ver  is not None): text += "  [Version]: {}\n".format(self.__ver)
         if (self.__mode is not None): text += "  [Mode]:    {}\n".format(self.__mode)
         if (self.__tty  is not None): text += "* [TTY]:     {} ({})\n".format(self.__tty, self.baudrate()); deep = 1
-        if (self.__auto_cmd is not None):   text += "  [Auto command]:     {}\n".format(self.__auto_cmd)
+        if (self.__auto_cmd_tty is not None):
+            text += "  [Auto command]:     {}\n".format(self.__auto_cmd)
+            text += "  [Auto command TTY]: {}\n".format(self.__auto_cmd_tty)
 
         if (deep):
             text += "\nPlease deeply check at * items\n"
@@ -659,7 +671,7 @@ class board(base):
     # auto_cmd_is_available
     #--------------------
     def auto_cmd_is_available(self):
-        if (self.__auto_cmd is None):
+        if (self.__auto_cmd_tty is None):
             return False
         else:
             return True
@@ -668,8 +680,8 @@ class board(base):
     # auto_cmd
     #--------------------
     def auto_cmd(self, cmd):
-        if (self.__auto_cmd is not None):
-            return self.run("{} {}".format(self.__auto_cmd, cmd))
+        if (self.__auto_cmd_tty is not None):
+            return self.run("{}/board/{} {} {}".format(self.top(), self.__auto_cmd, self.__auto_cmd_tty, cmd))
         else:
             return False
 

README
@@ -78,6 +78,13 @@
 	[update_style]:all	Update all files without asking.
 	[update_style]:ask	Ask one by one whether to update each file.
 
+
+	Some board supports auto_cmd to handle board. You can check
+	board/xxx/README whether your board supports it or not.
+	If the board is supporting auto_cmd, it needs to specify used serial.
+
+	[auto_cmd_tty]:/dev/ttyUSBx	use specified serial
+
 	ex) .renesas_bsp_rom_writer.xxx
 
 		-----------------

board/x5h_ironhide/README
@@ -51,23 +51,32 @@
 	fully remote way : invoking the "sdk_writer" tool will be done like before,
 	just without any user intervention on the board itself.
 
-	Setup :
+	You can use auto_cmd by settings [auto_cmd_tty] in
+	".renesas_bsp_rom_writer.x5h_ironhide" file.
+
+		> vi ${IMAGES}/.renesas_bsp_rom_writer.x5h_ironhide
+		...
+		[auto_cmd_tty]:/dev/ttyUSBx
+		...
+
+	see more detail
+
+		${TOP}/README
+
+Setup:
 
 	Power the board off, then set SW43 pin 8 to ON.
 
-	All the following instructions have to be done on the host computer.
+Option: [udev rule]
 
 	Install the udev rule that gives a unique and known name to the RL78
 	serial port.
-	sudo cp board/x5h_ironhide/linux/99-renesas-rcar-x5h.rules /lib/udev/rules.d
-	sudo udevadm control --reload-rules
-	sudo udevadm trigger
 
-	Copy the board control tool board/x5h_ironhide/linux/rcar_board_control_x5h
-	to an executable path (like /usr/bin).
+		> sudo cp board/x5h_ironhide/linux/99-renesas-rcar-x5h.rules /lib/udev/rules.d
+		> sudo udevadm control --reload-rules
+		> sudo udevadm trigger
 
-	Add the line "[auto_cmd]:rcar_board_control_x5h" (without the double quotes)
-	to the ~/Renesas/images/.renesas_bsp_rom_writer.x5h_ironhide file.
-
-	If you want to disable the automatic flashing, just remove the
-	"[auto_cmd]:rcar_board_control_x5h" line from the previous file.
+		> vi ${IMAGES}/.renesas_bsp_rom_writer.x5h_ironhide
+		...
+		[auto_cmd_tty]:/dev/ttyUSB-x5h-rl78
+		...

board/x5h_ironhide/linux/rcar_board_control_x5h
@@ -7,17 +7,18 @@
 
 set -e
 
-SERIAL_DEV=/dev/ttyUSB-x5h-rl78
+SERIAL_DEV=$1
+
 stty -F $SERIAL_DEV speed 921600 > /dev/null
 
 Usage () {
     echo "Usage: $0 on|off|flash|boot|debug"
 }
 
-if [[ $# -ne 1 ]]; then
+if [[ $# -ne 2 ]]; then
     Usage; exit -1
 fi
-case $1 in
+case $2 in
     "on"   )  CMD="power#on"  ;;
     "off"  )  CMD="power#off" ;;
     "flash")  CMD="md#74149"  ;;

script/v3h_condor.py
@@ -34,9 +34,9 @@ class board(base.board):
     #--------------------
     # init_with_mot
     #--------------------
-    def init_with_mot(self, soc, rom, ver, tty, baudrate=115200, board=None):
+    def init_with_mot(self, soc, rom, ver, tty, baudrate=115200, board=None, auto_cmd=None):
 
-        self.init(soc=soc, rom=rom, ver=ver, tty=tty, mode="mot", baudrate=baudrate, board=board)
+        self.init(soc=soc, rom=rom, ver=ver, tty=tty, mode="mot", baudrate=baudrate, board=board, auto_cmd=auto_cmd)
 
     #--------------------
     # __init__

script/x5h_ironhide.py
@@ -20,7 +20,8 @@ class board(v3h_condor.board):
     #--------------------
     def __init__(self):
 
-        self.init_with_mot("x5h", "sdk", "", "", baudrate=1843200)
+        self.init_with_mot("x5h", "sdk", "", "", baudrate=1843200,
+                           auto_cmd="x5h_ironhide/linux/rcar_board_control_x5h")
 
 #====================================
 #

commit b19829e8f80720aec1916f16f93cb060810ac3c8

script: base: remove wrong explanation
self.__auto_cmd_tty doesn't support "auto". Remove it.

Fixes: commit 0c88f2a49c2 ("x5h_ironhide: update auto_cmd")
Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>

script/base.py
@@ -301,7 +301,6 @@ class board(base):
         # for auto command
         self.__auto_cmd = auto_cmd	# path from ${TOP}/board/
         self.__auto_cmd_tty = None	# None:		not used
-                                  	# auto:		use udev
                                   	# /dev/ttyXX:	use specified serial
 
         self.confirm_location()